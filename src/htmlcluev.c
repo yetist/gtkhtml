c98275ddafb0802afd03baf56f964e2050e7f435
Anders Carlsson <andersca@src.gnome.org>
1999-10-23 18:36:29 +0000

    Add copyright headers
    svn path=/trunk/; revision=715

--- src/htmlcluev.c
+++ src/htmlcluev.c
@@ -1,3 +1,22 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 1997 Martin Jones (mjones@kde.org)
+              (C) 1997 Torben Weis (weis@kde.org)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
 #include "htmlcluealigned.h"
 #include "htmlcluev.h"
 #include "htmlobject.h"
@@ -333,7 +352,7 @@ html_cluev_append_right_aligned (HTMLClue *clue, HTMLClue *aclue)
 	
 	/* Insert clue in align list */
 	if (!HTML_CLUEV (clue)->alignRightList) {
-		HTML_CLUEV (clue)->alignRightList = aclue;
+		HTML_CLUEV (clue)->alignRightList = HTML_OBJECT (aclue);
 		HTML_CLUEALIGNED (aclue)->nextAligned = NULL;
 	}
 	else {
