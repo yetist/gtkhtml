975dce8c2ab99137c4cfe4e3927cebdc2682ee77
Radek Doulik <rodo@src.gnome.org>
2000-10-16 09:42:23 +0000

    ops, wrong version before
    svn path=/trunk/; revision=2248

--- src/htmlengine-save.c
+++ src/htmlengine-save.c
@@ -39,7 +39,6 @@ encode_entities (const gchar *input,
 		 guint len,
 		 guint *encoded_len_return)
 {
-	/* unicode_char_t uc; */
 	const gchar *p;
 	guchar *buffer = NULL;
 	guchar *out = NULL;
@@ -62,7 +61,6 @@ encode_entities (const gchar *input,
 			buffer = g_realloc (buffer, buffer_size);
 			out = &buffer[index];
 		}
-		// unicode_get_utf8 (p, &uc);
 
 		/* By default one have to encode at least '<', '>', '"' and '&'.  */
 
@@ -96,22 +94,9 @@ encode_entities (const gchar *input,
 			*out++ = 's';
 			*out++ = 'p';
 			*out++ = ';';
-		} /* else if (((uc >= 0x20) && (uc < 0x80)) 
-		     || (uc == '\n') || (uc == '\r') || (uc == '\t')) { */
-			/* Default case, just copy. */
+		}
 			*out++ = *p;
-			/* } else {
-			char buf[10], *ptr;
-
-			g_snprintf(buf, 9, "&#%d;", uc);
-
-			ptr = buf;
-			while (*ptr != 0)
-				*out++ = *ptr++;
-				} */
-
 		count++;
-		/* p = unicode_next_utf8 (p); */
 		p++;
 	}
 
