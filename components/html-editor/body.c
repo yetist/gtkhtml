61c9a6d5bc5a13f9cc853288626d82bf2d557a22
Radek Doulik <rodo@ximian.com>
2001-04-17 14:14:37 +0000

    as below
    2001-04-17  Radek Doulik  <rodo@ximian.com>
    
    	* toolbar.c (setup_color_combo): as below
    
    	* text.c (text_properties): as below
    
    	* body.c (ADD_COLOR): updated color_combo_new calling
    
    svn path=/trunk/; revision=3079

--- components/html-editor/body.c
+++ components/html-editor/body.c
@@ -164,7 +164,7 @@ body_properties (GtkHTMLControlData *cd, gpointer *set_data)
         html_color_alloc (color, cd->html->engine->painter); \
 	combo = color_combo_new (NULL, _("Automatic"), \
 				 &color->color, \
-				 "body_" g); \
+				 color_group_fetch ("body_" g, NULL)); \
         color_combo_set_color (COLOR_COMBO (combo), &data->color [ct]); \
         gtk_object_set_data (GTK_OBJECT (combo), "type", GINT_TO_POINTER (ct)); \
         gtk_signal_connect (GTK_OBJECT (combo), "changed", GTK_SIGNAL_FUNC (color_changed), data); \
