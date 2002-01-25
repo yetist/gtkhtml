5d6df3a3354a06b10ada0984cda0d580c23f66bf
Radek Doulik <rodo@src.gnome.org>
2002-01-25 22:12:35 +0000

    added body templates
    svn path=/trunk/; revision=4480

--- components/html-editor/body.c
+++ components/html-editor/body.c
@@ -36,13 +36,49 @@ struct _GtkHTMLEditBodyProperties {
 
 	GtkWidget *pixmap_entry;
 	GtkWidget *use_bg_image;
+	GtkWidget *option_template;
+	GtkWidget *combo [3];
 
 	GdkColor   color [HTMLColors];
 	gboolean   color_changed [HTMLColors];
 
+	gint template;
 	GtkHTML   *sample;
 };
 typedef struct _GtkHTMLEditBodyProperties GtkHTMLEditBodyProperties;
+
+#define TEMPLATES 3
+typedef struct {
+	gchar *name;
+	gchar *bg_pixmap;
+	GdkColor bg_color;
+	GdkColor text_color;
+	GdkColor link_color;
+} BodyTemplate;
+
+static BodyTemplate body_templates [TEMPLATES] = {
+	{
+		N_("None"),
+		NULL,
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+	},
+	{
+		N_("Test"),
+		NULL,
+		{0, 0x7fff, 0x7fff, 0xffff},
+		{0, 0xefff, 0xefff, 0xefff},
+		{0, 0xffff, 0xffff, 0},
+	},
+	{
+		N_("Test 1"),
+		ICONDIR "/bg1.png",
+		{0, 0xffff, 0xffff, 0xffff},
+		{0, 0x2fff, 0x2fff, 0x5fff},
+		{0, 0x7fff, 0x7fff, 0},
+	},
+};
 
 static void
 fill_sample (GtkHTMLEditBodyProperties *d)
@@ -116,13 +152,80 @@ entry_changed (GtkWidget *w, GtkHTMLEditBodyProperties *data)
 	fill_sample (data);
 }
 
+static void
+changed_template (GtkWidget *w, GtkHTMLEditBodyProperties *d)
+{
+	d->template = g_list_index (GTK_MENU_SHELL (w)->children, gtk_menu_get_active (GTK_MENU (w)));
+
+	gtk_entry_set_text (GTK_ENTRY (gnome_pixmap_entry_gtk_entry (GNOME_PIXMAP_ENTRY (d->pixmap_entry))),
+			    body_templates [d->template].bg_pixmap ? body_templates [d->template].bg_pixmap : "");
+
+	if (d->template) {
+		color_combo_set_color (COLOR_COMBO (d->combo [2]), &body_templates [d->template].bg_color);
+		color_combo_set_color (COLOR_COMBO (d->combo [0]), &body_templates [d->template].text_color);
+		color_combo_set_color (COLOR_COMBO (d->combo [1]), &body_templates [d->template].link_color);
+
+		d->color [HTMLBgColor] = body_templates [d->template].bg_color;
+		d->color_changed [HTMLBgColor] = TRUE;
+		d->color [HTMLLinkColor] = body_templates [d->template].link_color;
+		d->color_changed [HTMLLinkColor] = TRUE;
+		d->color [HTMLTextColor] = body_templates [d->template].text_color;
+		d->color_changed [HTMLTextColor] = TRUE;
+	} else {
+		color_combo_set_color (COLOR_COMBO (d->combo [2]),
+				       &html_colorset_get_color_allocated (d->cd->html->engine->painter,
+									   HTMLBgColor)->color);
+		color_combo_set_color (COLOR_COMBO (d->combo [0]),
+				       &html_colorset_get_color_allocated (d->cd->html->engine->painter,
+									   HTMLTextColor)->color);
+		color_combo_set_color (COLOR_COMBO (d->combo [1]),
+				       &html_colorset_get_color_allocated (d->cd->html->engine->painter,
+									   HTMLLinkColor)->color);
+
+		d->color [HTMLBgColor] = html_colorset_get_color (d->cd->html->engine->settings->color_set,
+								  HTMLBgColor)->color;
+		d->color_changed [HTMLBgColor] = FALSE;
+		d->color [HTMLLinkColor] = html_colorset_get_color (d->cd->html->engine->settings->color_set,
+								    HTMLLinkColor)->color;
+		d->color_changed [HTMLLinkColor] = FALSE;
+		d->color [HTMLTextColor] = html_colorset_get_color (d->cd->html->engine->settings->color_set,
+								    HTMLTextColor)->color;
+		d->color_changed [HTMLTextColor] = FALSE;
+	}
+
+	gtk_html_edit_properties_dialog_change (d->cd->properties_dialog);	
+	fill_sample (d);
+}
+
+static void
+fill_templates (GtkHTMLEditBodyProperties *d)
+{
+	GtkWidget *menu;
+	gint i;
+
+	menu = gtk_menu_new ();
+
+	for (i = 0; i < TEMPLATES; i ++) {
+		GtkWidget *item;
+
+		item = gtk_menu_item_new_with_label (_(body_templates [i].name));
+		gtk_menu_append (GTK_MENU (menu), item);
+		gtk_widget_show (item);
+		gdk_color_alloc (gdk_window_get_colormap (GTK_WIDGET (d->cd->html)->window), &body_templates [i].bg_color);
+		gdk_color_alloc (gdk_window_get_colormap (GTK_WIDGET (d->cd->html)->window), &body_templates [i].text_color);
+		gdk_color_alloc (gdk_window_get_colormap (GTK_WIDGET (d->cd->html)->window), &body_templates [i].link_color);
+	}
+	gtk_option_menu_set_menu (GTK_OPTION_MENU (d->option_template), menu);
+}
+
 GtkWidget *
 body_properties (GtkHTMLControlData *cd, gpointer *set_data)
 {
 	GtkHTMLEditBodyProperties *data = g_new0 (GtkHTMLEditBodyProperties, 1);
-	GtkWidget *hbox, *vbox, *frame, *combo, *table;
+	GtkWidget *hbox, *vbox, *frame, *combo, *table, *vb1;
 	GSList *group;
 	HTMLColor *color;
+	gint i;
 
 	*set_data = data;
 	data->cd = cd;
@@ -131,6 +234,18 @@ body_properties (GtkHTMLControlData *cd, gpointer *set_data)
 	gtk_container_border_width (GTK_CONTAINER (table), 3);
 	gtk_table_set_col_spacings (GTK_TABLE (table), 3);
 	gtk_table_set_row_spacings (GTK_TABLE (table), 2);
+
+	vb1 = gtk_vbox_new (FALSE, 0);
+	hbox = gtk_hbox_new (FALSE, 0);
+	gtk_container_set_border_width (GTK_CONTAINER (hbox), 3);
+	frame = gtk_frame_new (_("Template"));
+	data->option_template = gtk_option_menu_new ();
+	fill_templates (data);
+	gtk_signal_connect (GTK_OBJECT (gtk_option_menu_get_menu (GTK_OPTION_MENU (data->option_template))),
+			    "selection-done", changed_template, data);
+	gtk_box_pack_start (GTK_BOX (hbox), data->option_template, FALSE, TRUE, 0);
+	gtk_container_add (GTK_CONTAINER (frame), hbox);
+	gtk_box_pack_start_defaults (GTK_BOX (vb1), frame);
 	frame = gtk_frame_new (_("Background Image"));
 	vbox = gtk_vbox_new (FALSE, 2);
 	gtk_container_border_width (GTK_CONTAINER (vbox), 3);
@@ -159,8 +274,10 @@ body_properties (GtkHTMLControlData *cd, gpointer *set_data)
 
 	gtk_box_pack_start (GTK_BOX (vbox), data->use_bg_image, FALSE, FALSE, 0);
 	gtk_box_pack_start (GTK_BOX (vbox), data->pixmap_entry, FALSE, FALSE, 0);
+
 	gtk_container_add (GTK_CONTAINER (frame), vbox);
-	gtk_table_attach_defaults (GTK_TABLE (table), frame, 0, 1, 0, 1);
+	gtk_box_pack_start_defaults (GTK_BOX (vb1), frame);
+	gtk_table_attach_defaults (GTK_TABLE (table), vb1, 0, 1, 0, 1);
 	gtk_table_attach_defaults (GTK_TABLE (table), sample_frame (&data->sample), 0, 2, 1, 2);
 
 	frame = gtk_frame_new (_("Colors"));
@@ -168,11 +285,12 @@ body_properties (GtkHTMLControlData *cd, gpointer *set_data)
 	gtk_container_border_width (GTK_CONTAINER (vbox), 3);
 
 	group = NULL;
+	i = 0;
 #define ADD_COLOR(x, ct, g) \
         data->color [ct] = html_colorset_get_color_allocated (cd->html->engine->painter, ct)->color; \
         color = html_colorset_get_color (cd->html->engine->defaultSettings->color_set, ct); \
         html_color_alloc (color, cd->html->engine->painter); \
-	combo = color_combo_new (NULL, _("Automatic"), \
+	data->combo [i] = combo = color_combo_new (NULL, _("Automatic"), \
 				 &color->color, \
 				 color_group_fetch ("body_" g, cd)); \
         color_combo_set_color (COLOR_COMBO (combo), &data->color [ct]); \
@@ -181,7 +299,8 @@ body_properties (GtkHTMLControlData *cd, gpointer *set_data)
 	hbox = gtk_hbox_new (FALSE, 3); \
 	gtk_box_pack_start (GTK_BOX (hbox), combo, FALSE, FALSE, 0); \
 	gtk_box_pack_start (GTK_BOX (hbox), gtk_label_new (x), FALSE, FALSE, 0); \
-	gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0); \
+        i ++
 
 	ADD_COLOR (_("Text"), HTMLTextColor, "text");
 	ADD_COLOR (_("Link"), HTMLLinkColor, "link");
