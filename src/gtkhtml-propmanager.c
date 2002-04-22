c70b0162cfaae905d53443d685a41b47450256c1
Larry Ewing <lewing@ximian.com>
2002-04-22 14:29:18 +0000

    add font picker logic.
    2002-04-22  Larry Ewing  <lewing@ximian.com>
    
    	* gtkhtml-propmanager.c: add font picker logic.
    
    svn path=/trunk/; revision=4820

--- src/gtkhtml-propmanager.c
+++ src/gtkhtml-propmanager.c
@@ -1,8 +1,9 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /* This file is part of the GtkHTML library
 
-   Copyright (C) 2002 Ximian Inc.
+   Copyright (C) 2000, 2001, 2002 Ximian Inc.
    Authors:           Larry Ewing <lewing@ximian.com>
+                      Radek Doulik <rodo@ximian.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -80,8 +81,9 @@ gtk_html_propmanager_sync_ui (GtkHTMLPropmanager *pman)
 		gtk_widget_set_sensitive (GTK_WIDGET (priv->button_cfg_spell), priv->actual_prop->live_spell_check);
 	}
 
+
 #define SET_FONT(f,w) \
-        if (w) gnome_font_picker_set_font_name (GNOME_FONT_PICKER (w), priv->actual_prop-> f)
+        if (w) gnome_font_picker_set_font_name (GNOME_FONT_PICKER (w), priv->actual_prop-> f);
 
 	SET_FONT (font_var,       priv->variable);
 	SET_FONT (font_fix,       priv->fixed);
@@ -94,7 +96,7 @@ propmanager_client_notify (GConfClient *client, guint cnxn_id, GConfEntry *entry
 {
 	GtkHTMLPropmanager *pman = data;
 
-	printf ("GOT MILK??");
+	d (printf ("GOT MILK??"));
 
 	gtk_html_class_properties_load (pman->priv->actual_prop, client);
 	gtk_html_propmanager_sync_ui (pman);
@@ -104,6 +106,50 @@ static void
 propmanager_changed (GtkWidget *widget, GtkHTMLPropmanager *pman)
 {
 	gtk_signal_emit (GTK_OBJECT (pman), signals[CHANGED]);
+}
+
+static void
+propmanager_sensitivity_changed (GtkWidget *widget, GtkHTMLPropmanager *pman)
+{
+	GtkHTMLPropmanagerPrivate *priv;
+	g_return_if_fail (pman != NULL);
+
+	priv = pman->priv;
+	gtk_widget_set_sensitive (priv->button_cfg_spell,
+				  gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->live_spell_check)));
+}
+
+#define SELECTOR(x) GTK_FONT_SELECTION_DIALOG (GNOME_FONT_PICKER (x)->font_dialog)
+
+static void
+propmanager_picker_clicked (GtkWidget *w, gpointer proportional)
+{
+	gchar *mono_spaced [] = { "c", "m", NULL };
+
+	if (GPOINTER_TO_INT (proportional))
+		gtk_font_selection_dialog_set_filter (SELECTOR (w),
+						      GTK_FONT_FILTER_BASE, GTK_FONT_ALL,
+						      NULL, NULL, NULL, NULL,
+						      mono_spaced, NULL);
+}
+
+static void
+propmanager_child_destroyed (GtkWidget *w, GtkHTMLPropmanager *pman)
+{
+
+}
+
+static GtkWidget *
+propmanager_add_picker (GtkWidget *picker, gboolean proportional, gboolean *found)
+{
+	if (picker) {
+		gtk_signal_connect (GTK_OBJECT (picker), "clicked", propmanager_picker_clicked,
+				    GINT_TO_POINTER (proportional));
+		gtk_signal_connect (GTK_OBJECT (picker), "destroy", propmanager_child_destroyed,
+				    NULL);
+		*found = TRUE;
+	}
+	return picker;
 }
 
 gboolean
@@ -147,13 +193,18 @@ gtk_html_propmanager_set_xml (GtkHTMLPropmanager *pman, GladeXML *xml)
 		found_widget = TRUE;
 	}
 
-	priv->variable        = glade_xml_get_widget (xml, "screen_variable");
-	priv->variable_print  = glade_xml_get_widget (xml, "print_variable");
-	priv->fixed           = glade_xml_get_widget (xml, "screen_fixed");
-	priv->fixed_print     = glade_xml_get_widget (xml, "print_fixed");
+	priv->variable = propmanager_add_picker (glade_xml_get_widget (xml, "screen_variable"),
+						 TRUE, &found_widget);
+	priv->variable_print = propmanager_add_picker (glade_xml_get_widget (xml, "print_variable"),
+						       TRUE, &found_widget);
+	priv->fixed = propmanager_add_picker (glade_xml_get_widget (xml, "screen_fixed"),
+					      FALSE, &found_widget);
+	priv->fixed_print = propmanager_add_picker (glade_xml_get_widget (xml, "print_fixed"),
+						    FALSE, &found_widget);
 
-	gconf_client_notify_add (client, GTK_HTML_GCONF_DIR, propmanager_client_notify, 
-				 pman, NULL, &gconf_error);
+	priv->notify_id = gconf_client_notify_add (client, GTK_HTML_GCONF_DIR, 
+						   propmanager_client_notify, 
+						   pman, NULL, &gconf_error);
 	if (gconf_error)
 		g_warning ("gconf error: %s\n", gconf_error->message);
 				 
@@ -165,22 +216,69 @@ gtk_html_propmanager_set_xml (GtkHTMLPropmanager *pman, GladeXML *xml)
 	return found_widget;
 }
 
+static gchar *
+get_attr (gchar *font_name, gint n)
+{
+    gchar *s, *end;
+
+    /* Search paramether */
+    for (s=font_name; n; n--,s++)
+	    s = strchr (s,'-');
+
+    if (s && *s != 0) {
+	    end = strchr (s, '-');
+	    if (end)
+		    return g_strndup (s, end - s);
+	    else
+		    return g_strdup (s);
+    } else
+	    return g_strdup ("Unknown");
+}
+
 void
 gtk_html_propmanager_apply (GtkHTMLPropmanager *pman)
 {
 	GtkHTMLPropmanagerPrivate *priv;
+	gchar *size_str;
+
 	g_return_if_fail (pman != NULL);
 
 	priv = pman->priv;
 	
-	if (priv->anim_check)
+	if (priv->anim_check) {
 		priv->actual_prop->animations = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->anim_check));
-
+		if (priv->actual_prop->animations) {
+			d (g_warning ("FUCKING HELL"));
+		} else {
+			d (g_warning ("FUCKING HEAVEN"));
+		}
+	}
 	if (priv->magic_check)
 		priv->actual_prop->magic_links = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->magic_check));
 
 	if (priv->live_spell_check)
 		priv->actual_prop->live_spell_check = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->live_spell_check));
+
+#define APPLY(f,s,w) \
+        if (w) { \
+	        g_free (priv->actual_prop-> f); \
+	        priv->actual_prop-> f = g_strdup (gnome_font_picker_get_font_name (GNOME_FONT_PICKER (w))); \
+	        size_str = get_attr (gnome_font_picker_get_font_name (GNOME_FONT_PICKER (w)), 7); \
+                if (!strcmp (size_str, "*")) { \
+                          g_free (size_str); \
+	                  size_str = get_attr (gnome_font_picker_get_font_name (GNOME_FONT_PICKER (w)), 8); \
+                          priv->actual_prop-> f ## _points = TRUE; \
+                } else { \
+                          priv->actual_prop-> f ## _points = FALSE; \
+                } \
+	        priv->actual_prop-> s = atoi (size_str); \
+	        g_free (size_str); \
+	}
+
+	APPLY (font_var,       font_var_size,       priv->variable);
+	APPLY (font_fix,       font_fix_size,       priv->fixed);
+	APPLY (font_var_print, font_var_size_print, priv->variable_print);
+	APPLY (font_fix_print, font_fix_size_print, priv->fixed_print);
 
 	gtk_html_class_properties_update (priv->actual_prop, pman->client,
 					  priv->saved_prop);
