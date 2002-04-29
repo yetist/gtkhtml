20084da4c1efdc94aa3fbbaf769a69da11e0156e
Larry Ewing <lewing@ximian.com>
2002-04-29 06:31:41 +0000

    add cast.
    2002-04-29  Larry Ewing  <lewing@ximian.com>
    
    	* gtkhtml-propmanager.c (gtk_html_propmanager_real_changed): add cast.
    
    svn path=/trunk/; revision=4844

--- src/gtkhtml-propmanager.c
+++ src/gtkhtml-propmanager.c
@@ -500,7 +500,7 @@ gtk_html_propmanager_real_changed (GtkHTMLPropmanager *pman)
 
 		d (g_warning ("spell sensitivity changed = %d", priv->actual_prop->live_spell_check));
 
-		sensitive = gtk_toggle_button_get_active (priv->live_spell_check);
+		sensitive = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->live_spell_check));
 		gtk_widget_set_sensitive (GTK_WIDGET (priv->live_spell_options), sensitive);
 	}
 }
