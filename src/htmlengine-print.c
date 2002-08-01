004e29da69cbcefe8058788c7b9b3bc28f32828b
Radek Doulik <rodo@src.gnome.org>
2002-08-01 22:10:16 +0000

    removed //
    svn path=/trunk/; revision=5263

--- src/htmlengine-print.c
+++ src/htmlengine-print.c
@@ -182,7 +182,6 @@ print_with_header_footer (HTMLEngine *engine,
 		/* printf ("min_width %d page %d\n", min_width, page_width); */
 		if (min_width > page_width) {
 			HTML_PRINTER (printer)->scale = MAX (0.5, ((gdouble) page_width) / min_width);
-			//html_font_manager_clear_font_cache (html_engine_font_manager_with_painter (engine, printer));
 			html_object_change_set_down (engine->clue, HTML_CHANGE_ALL);
 			html_engine_calc_size (engine, NULL);
 			/* printf ("scale %lf\n", HTML_PRINTER (printer)->scale);
