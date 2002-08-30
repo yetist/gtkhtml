8bd0757e01d14aba6d893204554b8d29e2aa44f4
Radek Doulik <rodo@ximian.com>
2002-08-30 18:15:56 +0000

    paste text instead of inserting it
    2002-08-30  Radek Doulik  <rodo@ximian.com>
    
    	* gtkhtml.c (insert_tab_or_next_cell): paste text instead of
    	inserting it
    
    svn path=/trunk/; revision=5316

--- src/gtkhtml.c
+++ src/gtkhtml.c
@@ -3799,14 +3799,11 @@ insert_tab_or_next_cell (GtkHTML *html)
 {
 	HTMLEngine *e = html->engine;
 	if (!html_engine_next_cell (e, TRUE)) {
-		if (!html_engine_is_selection_active (e)) {
-			if (html_clueflow_tabs (HTML_CLUEFLOW (e->cursor->object->parent), e->painter))
-				html_engine_insert_text (e, "\t", 1);
-			else
-				html_engine_insert_text (e, "\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0", 4);
-			return TRUE;
-		}
-		return FALSE;
+		if (html_clueflow_tabs (HTML_CLUEFLOW (e->cursor->object->parent), e->painter))
+			html_engine_paste_text (e, "\t", 1);
+		else
+			html_engine_paste_text (e, "\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0", 4);
+		return TRUE;
 	}
 
 	return TRUE;
