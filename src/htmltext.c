256b93376ca48a120e930d578cc19ec09d385c2d
Radek Doulik <rodo@ximian.com>
2004-07-19 13:40:37 +0000

    save <TT> (save_close_attrs): save </TT>
    2004-07-19  Radek Doulik  <rodo@ximian.com>
    
    	* htmltext.c (save_open_attrs): save <TT>
    	(save_close_attrs): save </TT>
    
    svn path=/trunk/; revision=7396

--- src/htmltext.c
+++ src/htmltext.c
@@ -1483,6 +1483,7 @@ save_open_attrs (HTMLEngineSaveState *state, GSList *attrs)
 
 	for (; attrs; attrs = attrs->next) {
 		PangoAttribute *attr = (PangoAttribute *) attrs->data;
+		HTMLEngine *e = state->engine;
 		gchar *tag = NULL;
 		gboolean free_tag = FALSE;
 
@@ -1508,9 +1509,15 @@ save_open_attrs (HTMLEngineSaveState *state, GSList *attrs)
 				}
 			}
 			break;
-		case PANGO_ATTR_FAMILY:
-			/* TODO */
-			break;
+		case PANGO_ATTR_FAMILY: {
+			PangoAttrString *family_attr = (PangoAttrString *) attr;
+
+			if (!strcasecmp (e->painter->font_manager.fixed.face
+					? e->painter->font_manager.fixed.face : "Monospace",
+					family_attr->value))
+				tag = "<TT>";
+		}
+		break;
 		case PANGO_ATTR_FOREGROUND: {
 			PangoAttrColor *color = (PangoAttrColor *) attr;
 			tag = g_strdup_printf ("<FONT COLOR=\"#%02x%02x%02x\">",
@@ -1540,6 +1547,7 @@ save_close_attrs (HTMLEngineSaveState *state, GSList *attrs)
 {
 	for (; attrs; attrs = attrs->next) {
 		PangoAttribute *attr = (PangoAttribute *) attrs->data;
+		HTMLEngine *e = state->engine;
 		gchar *tag = NULL;
 
 		switch (attr->klass->type) {
@@ -1565,8 +1573,15 @@ save_close_attrs (HTMLEngineSaveState *state, GSList *attrs)
 		case PANGO_ATTR_FOREGROUND:
 			tag = "</FONT>";
 			break;
-		case PANGO_ATTR_FAMILY:
-			break;
+		case PANGO_ATTR_FAMILY: {
+			PangoAttrString *family_attr = (PangoAttrString *) attr;
+
+			if (!strcasecmp (e->painter->font_manager.fixed.face
+					? e->painter->font_manager.fixed.face : "Monospace",
+					family_attr->value))
+				tag = "</TT>";
+		}
+		break;
 		default:
 			break;
 		}
