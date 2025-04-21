7da701c6f37d5c96ba7ad15489e1aa2a4d54f75b
Xiaotian Wu <yetist@gmail.com>
2025-04-21 22:06:25 +0800

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,41 +1,63 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+ECHO_C=
+ECHO_N=
+case $(echo -n x) in
+-n*)
+  case $(echo 'x\c') in
+  *c*) ;;
+  *) ECHO_C='\c' ;;
+  esac
+  ;;
+*)
+  ECHO_N='-n'
+  ;;
+esac
 
-PKG_NAME="gtkhtml"
-REQUIRED_AUTOCONF_VERSION=2.58
-REQUIRED_AUTOMAKE_VERSION=1.9
-REQUIRED_LIBTOOL_VERSION=2.2
-REQUIRED_INTLTOOL_VERSION=0.36.3
-
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-(test -f $srcdir/configure.ac \
-  && test -d $srcdir/gtkhtml \
-  && test -f $srcdir/gtkhtml/gtkhtml.h) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gtkhtml directory"
-    exit 1
+# some terminal codes ...
+boldface="$(tput bold 2>/dev/null)"
+normal="$(tput sgr0 2>/dev/null)"
+printbold() {
+  echo $ECHO_N "$boldface" $ECHO_C
+  echo "$@"
+  echo $ECHO_N "$normal" $ECHO_C
 }
 
-gnome_autogen=
-gnome_datadir=
+printbold Running libtoolize...
+libtoolize --force --copy
+printbold Running glib-gettextize...
+glib-gettextize --force --copy
+printbold Running intltoolize...
+intltoolize --force --copy --automake
+printbold Running gtkdocize...
+gtkdocize --copy
+printbold Running aclocal...
+aclocal -I m4
+printbold Running autoconf...
+autoconf
+printbold Running autoheader...
+autoheader
 
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
-
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
+if [ -f COPYING ]; then
+  cp -pf COPYING COPYING.autogen_bak
+fi
+if [ -f INSTALL ]; then
+  cp -pf INSTALL INSTALL.autogen_bak
 fi
 
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
+printbold Running automake...
+automake --gnu --add-missing --force --copy
+
+if [ -f COPYING.autogen_bak ]; then
+  cmp COPYING COPYING.autogen_bak >/dev/null || cp -pf COPYING.autogen_bak COPYING
+  rm -f COPYING.autogen_bak
+fi
+if [ -f INSTALL.autogen_bak ]; then
+  cmp INSTALL INSTALL.autogen_bak >/dev/null || cp -pf INSTALL.autogen_bak INSTALL
+  rm -f INSTALL.autogen_bak
+fi
+
+conf_flags="--prefix=/usr --enable-maintainer-mode --enable-gtk-doc"
+
+printbold Running ./configure $conf_flags "$@" ...
+./configure $conf_flags "$@" &&
+  echo Now type \`make\' to compile gtkhtml
